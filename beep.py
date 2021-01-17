11fb9765eded9b0b05c85acc83f70b6fa75d1c02
Georg101 <77585051+Georg101@users.noreply.github.com>
2021-01-17 19:20:59 +0100

    Base to interpreting final score

---
+++ beep.py
@@ -0,0 +1,37 @@
+import winsound
+import pyttsx3
+engine = pyttsx3.init()
+
+
+#C
+engine.say("C4")
+engine.runAndWait()
+winsound.Beep(261, 1000)
+#D
+engine.say("D4")
+engine.runAndWait()
+winsound.Beep(293, 1000)
+#E
+engine.say("E4")
+engine.runAndWait()
+winsound.Beep(330, 1000)
+#F
+engine.say("F4")
+engine.runAndWait()
+winsound.Beep(349, 1000)
+#G
+engine.say("G4")
+engine.runAndWait()
+winsound.Beep(392, 1000)
+#A
+engine.say("A4")
+engine.runAndWait()
+winsound.Beep(440, 1000)
+#H
+engine.say("H4")
+engine.runAndWait()
+winsound.Beep(494, 1000)
+#C5
+engine.say("C5")
+engine.runAndWait()
+winsound.Beep(523, 1000)
