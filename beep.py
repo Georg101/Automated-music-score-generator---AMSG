fdf4382490af4b1bf9da640a0bc45a258fb01cca
Georg101 <77585051+Georg101@users.noreply.github.com>
2021-01-24 18:41:15 +0100

    score_reader.py
    This program reads and plays the file notes.score

--- beep.py
+++ beep.py
@@ -1,37 +1,41 @@
 import winsound
-import pyttsx3
-engine = pyttsx3.init()
+f = open("notes.score", "r")
 
+read = f.read()
 
-#C
-engine.say("C4")
-engine.runAndWait()
-winsound.Beep(261, 1000)
-#D
-engine.say("D4")
-engine.runAndWait()
-winsound.Beep(293, 1000)
-#E
-engine.say("E4")
-engine.runAndWait()
-winsound.Beep(330, 1000)
-#F
-engine.say("F4")
-engine.runAndWait()
-winsound.Beep(349, 1000)
-#G
-engine.say("G4")
-engine.runAndWait()
-winsound.Beep(392, 1000)
-#A
-engine.say("A4")
-engine.runAndWait()
-winsound.Beep(440, 1000)
-#H
-engine.say("H4")
-engine.runAndWait()
-winsound.Beep(494, 1000)
-#C5
-engine.say("C5")
-engine.runAndWait()
-winsound.Beep(523, 1000)
+if read[3] == 'e':
+    print('c')
+else:
+    print('nic')
+
+count = 0
+
+while count <30 :
+    
+    if read[count] == "c":
+        print("c")
+        winsound.Beep(261, 500)
+    
+    elif read[count] == "d":
+        print("d")
+        winsound.Beep(293, 500)
+
+    elif read[count] == "e":
+        print("e")
+        winsound.Beep(330, 500)
+    elif read[count] == "f":
+        print("f")
+        winsound.Beep(349, 500)
+
+    elif read[count] == "g":
+        print("g")
+        winsound.Beep(392, 500)
+        
+
+    elif read[count] == "a":
+        print("a")
+        winsound.Beep(440, 500)
+    else:
+        print("h")
+        winsound.Beep(494, 500)
+    count = count + 1
