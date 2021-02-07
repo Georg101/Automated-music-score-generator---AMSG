c8a8129f0290c6dee8eb215a084a805e0221a139
Georg101 <77585051+Georg101@users.noreply.github.com>
2021-02-07 17:54:32 +0100

    The main and only file. The Alfa and Omega.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import random
+import time
+import re
+import winsound
 
 f = open("notes.score", "w")
 y = (random.randint(0,6))
@@ -30,9 +33,9 @@ elif y == 5:
     f.write("a")
 else:
     print("h")
-    f.write("h") 
+    f.write("h")
 
-#this part writes 20 notes to the file notes.txt
+#this part writes x notes to the file notes.score
 count = 0
 
 while count <how_many_notes-2 :
@@ -99,3 +102,105 @@ f.write("c")
 
     
 f.close()
+
+
+#this part checks for "cd", "dc", "hc", "ch"
+f = open("notes.score", "r")
+read = f.read()
+text = read
+if re.search(r"ch", text):
+    print("ch Match found")
+else:
+    print("ch Match not found")
+
+if re.search(r"dc", text):
+    print("dc Match found")
+else:
+    print("dc Match not found")
+
+if re.search(r"cd", text):
+    print("cd Match found")
+else:
+    print("cd Match not found")
+
+if re.search(r"hc", text):
+    print("hc Match found")
+else:
+    print("hc Match not found")
+
+# this part deletes the "cd", "dc", "hc", "ch" patterns
+
+
+f = open('notes.score', "r+")
+a = open('notes.v1', "w")
+
+data = f.read().replace('cd', '')
+a.write(data)
+print(data)
+f.close()
+a.close()
+
+f = open('notes.v1', "r+")
+b = open('notes.v2', "w")
+
+data = f.read().replace('dc', '')
+b.write(data)
+print(data)
+f.close()
+b.close()
+
+f = open('notes.v2', "r+")
+c = open('notes.v3', "w")
+
+data = f.read().replace('hc', '')
+c.write(data)
+print(data)
+f.close()
+c.close()
+
+f = open('notes.v3', "r+")
+d = open('notes.v4', "w")
+
+data = f.read().replace('ch', '')
+d.write(data)
+print(data)
+f.close()
+d.close()
+
+
+# this part plays the "notes.v4"
+play = open("notes.v4", "r")
+read = play.read()
+count = 0
+
+while count <1000 :
+    
+    if read[count] == "c":
+        print("c")
+        winsound.Beep(261, 500)
+    
+    elif read[count] == "d":
+        print("d")
+        winsound.Beep(293, 500)
+
+    elif read[count] == "e":
+        print("e")
+        winsound.Beep(330, 500)
+    elif read[count] == "f":
+        print("f")
+        winsound.Beep(349, 500)
+
+    elif read[count] == "g":
+        print("g")
+        winsound.Beep(392, 500)
+        
+
+    elif read[count] == "a":
+        print("a")
+        winsound.Beep(440, 500)
+    else:
+        print("h")
+        winsound.Beep(494, 500)
+    count = count + 1
+
+
